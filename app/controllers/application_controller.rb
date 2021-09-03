54e156a45052c8fa757ad5aa4c896cab00e2abd6
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-03 11:54:33 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/teams' do
+        @team = Team.new(params[:team][:name], params[:team][:motto])
+        params[:team][:members].each do |member|
+            Member.new(member)
+        end
+        
+        @member = Member.all
+        # binding.pry
+
+        erb :team
+    end
 
 
 end
